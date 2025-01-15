883a7248e94215c27ccd96f05d35cd0a29aca15d
Kiril Videlov <kiril@videlov.com>
2025-01-15 18:57:57 +0100

    Setup structs for the new API getting the branches for a given stack

--- crates/but-workspace/src/lib.rs
+++ crates/but-workspace/src/lib.rs
@@ -8,6 +8,13 @@
 //!       multiple branches to be perceived in one worktree, by merging multiple branches together.
 //!     - Currently, there is only one workspace per repository, but this is something we intend to change
 //!       in the future to facilitate new use cases.
+//!
+//! * **Stack**
+//!   - GitButler implements the concept of a branch stack. This is essentially a collection of "heads"
+//!     (pseudo branches) that contain each other.
+//!   - Always contains at least one branch.
+//!   - High level documentation here: https://docs.gitbutler.com/features/stacked-branches
+//!
 
 use anyhow::Result;
 use bstr::BString;
@@ -41,6 +48,78 @@ pub fn stacks(gb_dir: &Path) -> Result<Vec<StackEntry>> {
             branch_names: stack.heads().into_iter().map(Into::into).collect(),
         })
         .collect())
+}
+
+/// Represents the state a commit could be in for the purposes of rendering the UI.
+#[derive(Debug, Clone)]
+pub enum CommitState {
+    /// The commit is only local
+    LocalOnly,
+    /// The commit is also present at the remote [`StackBranch::upstream_refrence`].
+    /// This is the commit state if:
+    ///  - The commit has been pushed to the remote
+    ///  - The commit has been copied from a remote commit (when applying a remote branch)
+    ///
+    /// This variant carries the remote commit id.
+    /// The `remote_commit_id` may be the same as the `id` or it may be different if the local commit has been rebased or updated in another way.
+    LocalAndRemote(gix::ObjectId),
+    /// The commit is considered integrated.
+    /// This should happen when this commit or the contents of this commit is already part of the base.
+    Integrated,
+}
+
+/// Commit that is a part of a [`StackBranch`] and, as such, containing state derived in relation to the specific branch.
+#[derive(Debug, Clone)]
+pub struct StackBranchCommit {
+    /// The OID of the commit.
+    pub id: gix::ObjectId,
+    /// The message of the commit.
+    pub message: BString,
+    /// Whether the commit is in a conflicted state.
+    /// Conflicted state of a commit is a GitButler concept.
+    /// GitButler will perform rebasing/reordering etc without interruptions and flag commits as conflicted if needed.
+    /// Conflicts are resolved via the Edit Mode mechanism.
+    pub has_conflicts: bool,
+    /// Represents wether the the commit is considered integrated, local only,
+    /// or local and remote with respect to the branch it belongs to.
+    /// Note that remote only commits in the context of a branch are expressed with the [`UpstreamCommit`] struct instead of this.
+    pub state: CommitState,
+}
+
+/// Commit that is only at the remote.
+/// Unlike the `Commit` struct, there is no knowledge of GitButler concepts like conflicted state etc.
+#[derive(Debug, Clone)]
+pub struct UpstreamCommit {
+    /// The OID of the commit.
+    pub id: gix::ObjectId,
+    /// The message of the commit.
+    pub message: BString,
+}
+
+/// Replesents a branch in a [`gitbutler_stack::Stack`]. It contains commits derived from the local pseudo branch and it's respective remote
+#[derive(Debug, Clone)]
+pub struct StackBranch {
+    /// The name of the branch.
+    pub name: BString,
+    /// Upstream reference, e.g. `refs/remotes/origin/base-branch-improvements`
+    pub upstream_refrence: Option<BString>,
+    /// List of commits beloning to this branch. Ordered from newest to oldest.
+    /// Created from the local pseudo branch (head currently stored in the TOML file)
+    pub commits: Vec<StackBranchCommit>,
+    /// List of commits that exist **only** on the upstream branch. Ordered from newest to oldest.
+    /// Created from the tip of the local tracking branch eg. refs/remotes/origin/my-branch -> refs/heads/my-branch
+    pub upstream_commits: Vec<UpstreamCommit>,
+    /// Description of the branch.
+    /// This is provided by the UI and can include arbitrary utf8 data, eg. markdown etc.
+    pub description: Option<String>,
+    /// The pull(merge) request associated with the branch, or None if no such entity has not been created.
+    /// This is provided by the UI which is also responsible for creating Pull Requests.
+    pub pr_number: Option<usize>,
+}
+
+/// Provides the relevant details of a particular [`gitbutler_stack::Stack`]
+pub fn stack_branches() -> Result<()> {
+    Ok(())
 }
 
 fn state_handle(gb_state_path: &Path) -> VirtualBranchesHandle {
